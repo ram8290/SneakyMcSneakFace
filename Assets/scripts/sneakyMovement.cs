64f571888958319aee3b1c45b1c53b6fe39f27f2
Ricardo Martinez <ram8290@uat.edu>
2019-02-17 23:33:51 -0700

    Initial commit

---
+++ Assets/scripts/sneakyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sneakyMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public Transform tf;
+    public float moveSpeed;
+    public float turnSpeed;
+    public float angle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.up * (Time.deltaTime * moveSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            rb.AddForce(-transform.up * (Time.deltaTime * moveSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            angle += (turnSpeed * Time.deltaTime);
+            tf.rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            angle -= (turnSpeed * Time.deltaTime);
+            tf.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
